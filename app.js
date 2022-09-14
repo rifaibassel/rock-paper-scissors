f455cc67157bda46857b45e9414c3dfd7b246f17
Ahmad Alrefai <bassel@basselalrefai.com>
2022-09-14 19:36:11 -0400

    Create function to play a single round

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ function getComputerChoice() {
   const choiceArr = ['rock', 'paper', 'scissors'];
   return choiceArr[Math.floor(Math.random() * 3)];
 }
+
+function playSingleRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    playerSelection.charAt(0).toUpperCase();
+    return `You Win! ${playerSelection} beats ${computerSelection}.`;
+  } else if (computerSelection === playerSelection) {
+    return 'Tie!';
+  } else {
+    `You Lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
