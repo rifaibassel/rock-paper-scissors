2461cd0f614e075800764746b7a5e808fcffee26
Ahmad Alrefai <bassel@basselalrefai.com>
2022-09-14 19:52:31 -0400

    Create game function to play 5 rounds and find winner

--- app.js
+++ app.js
@@ -25,3 +25,38 @@ function playSingleRound(playerSelection, computerSelection) {
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+function game() {
+  let playerCounter = 0;
+  let computerCounter = 0;
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt('Choose your weapon!');
+    let result = playSingleRound(playerSelection, computerSelection);
+    if (
+      result ===
+      `You Win! ${capitalize(playerSelection)} beats ${capitalize(
+        computerSelection
+      )}.`
+    ) {
+      console.log(result);
+      playerCounter++;
+    } else if ((result = 'Tie!')) {
+      console.log(result);
+    } else {
+      console.log(result);
+      computerCounter++;
+    }
+  }
+  if (computerCounter > playerCounter) {
+    console.log(
+      `You Lose! The Computer has scored ${computerCounter} while you scored ${playerCounter}.`
+    );
+  } else if (computerCounter < playerCounter) {
+    console.log(
+      `You Win! You have scored ${playerCounter} while the computer scored ${computerCounter}.`
+    );
+  } else {
+    console.log('Tie!');
+  }
+}
